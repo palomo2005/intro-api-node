1affe7cd06d438d619e2cf1b7b5e892398225be3
palomo2005 <gabriel.palomo@etec.sp.gov.br>
2025-03-31 20:39:55 -0300

    Aula 31/03

--- src/controllers/skins.js
+++ src/controllers/skins.js
@@ -16,4 +16,58 @@ module.exports = {
             });
         }
     }
+},
+
+module.exports = {
+    async cadastrarSkins(request, response) {
+        try {
+            return response.status(200).json ({
+                sucesso: true,
+                mensagem: 'Lista de skins.',
+                dados: null
+            });
+        }catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição',
+                dados: error.menssage
+            });
+        }
+    }
+},
+
+module.exports = {
+    async editarSkins(request, response) {
+        try {
+            return response.status(200).json ({
+                sucesso: true,
+                mensagem: 'Lista de skins.',
+                dados: null
+            });
+        }catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição',
+                dados: error.menssage
+            });
+        }
+    }
+},
+
+module.exports = {
+    async apagarSkins(request, response) {
+        try {
+            return response.status(200).json ({
+                sucesso: true,
+                mensagem: 'Lista de skins.',
+                dados: null
+            });
+        }catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição',
+                dados: error.menssage
+            });
+        }
+    }
 }
