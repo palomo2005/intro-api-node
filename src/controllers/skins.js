f94678bf90e487182dc9020ea0b55c579ff4de19
palomo2005 <gabriel.palomo@etec.sp.gov.br>
2025-05-03 12:27:43 -0300

    Ola

--- src/controllers/skins.js
+++ src/controllers/skins.js
@@ -24,11 +24,9 @@ module.exports = {
     },
     async cadastrarSkins(request, response) {
         try {
-            return response.status(200).json({
-                sucesso: true,
-                mensagem: 'Cadastro de skins.',
-                dados: null
-            });
+
+
+            
         } catch (error) {
             return response.status(500).json({
                 sucesso: false,
